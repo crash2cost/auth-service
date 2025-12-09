bc260154b9a3716e12a81417ec5e1ec4db732dbe
ido levi <ido1112levi@gmail.com>
2025-12-09 15:57:27 +0200

    Add demo controller endpoint

---
+++ src/main/java/io/github/mrlevi1112/authservice/controller/DemoController.java
@@ -0,0 +1,16 @@
+package io.github.mrlevi1112.authservice.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/demo")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.ok("Hello from secured endpoint! If you see this, your Token works.");
+    }
+}
