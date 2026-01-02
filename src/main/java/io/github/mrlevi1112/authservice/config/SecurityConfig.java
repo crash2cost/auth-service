5683db3ddec57435bc7833e24d2cd53eb5095a58
ido levi <ido1112levi@gmail.com>
2026-01-02 08:24:48 +0200

    config: Disable CORS for authentication service

--- src/main/java/io/github/mrlevi1112/authservice/config/SecurityConfig.java
+++ src/main/java/io/github/mrlevi1112/authservice/config/SecurityConfig.java
@@ -32,6 +32,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+            .cors(AbstractHttpConfigurer::disable)
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
                 .requestMatchers(AuthServiceConstants.Security.AUTH_ENDPOINT_PATTERN).permitAll()
